51b9a2c6b0af1836961877e3927bfe9974f5f10f
superdim20 <dimasyudistiras20@gmail.com>
2025-01-30 20:50:31 +0700

    ubah sidebar

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,8 @@ Route::get('/dashboard', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('publisher', App\Http\Controllers\PublisherController::class)->middleware('auth');
-
+// Route::resource('publisher', App\Http\Controllers\PublisherController::class)->middleware('auth');
+Route::get('/publisher', [PublisherController::class, 'index'])->name('publisher.index');
+Route::get('/publisher/create', [PublisherController::class, 'create'])->name('publisher.create');
+Route::post('/publisher', [PublisherController::class, 'store'])->name('publisher.store');
+Route::delete('/publisher/{publisher}', [PublisherController::class, 'destroy'])->name('publisher.destroy');
